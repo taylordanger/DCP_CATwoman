ccd9f792c3e6422713a29584fef50402aebbf3ce
taylordanger <taylordangersheppard@gmail.com>
2023-04-07 20:25:32 -0500

    Simple_TCP_Client

---
+++ TCP_Client.py
@@ -0,0 +1,17 @@
+import socket
+target_host = "www.google.com"
+target_port = 80
+
+#create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect that fool
+client.connect((target_host,target_port))
+
+client.send("GET / HTTP/1.1\rnHOST: google.com\r\n\r\n")
+
+#get that datatatataatatattaa
+
+ response = clientrecv(4096)
+
+ print(response)
